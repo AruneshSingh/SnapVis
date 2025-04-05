c2bb01d5b25a81622de01f5326f7152978d845d9
Arunesh Singh <43724007+AruneshSingh@users.noreply.github.com>
2025-04-05 22:45:25 +0530

    Initial Commit

---
+++ SnapVis/SnapVisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapVisApp.swift
+//  SnapVis
+//
+//  Created by Arunesh Singh on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapVisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
